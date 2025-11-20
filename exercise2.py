80091af718c4f11b3b542866f39e71e5ddab7dc4
Tolu Adefemi <toluadefemi77@gmail.com>
2025-11-20 10:41:43 +0000

    Exercise 2 completion

--- exercise2.py
+++ exercise2.py
@@ -9,9 +9,20 @@
 # Side effects
 #    - None
 def count_characters(text, character):
-    pass
+    count = 0 # task 1 
+
+    for char in text: #task 2
+        if char == character: 
+            count +=1 # task 3
+    return count # task 4
+
 
 print(count_characters("Its a fantastic day", "a"))
+print(count_characters("Checking if answer works well", "w"))
 
 # Tasks 
-# - 
+# Create a variable to track number of characters 
+# Find the characters in the string (Likely through looping through)
+# Update the tracker each time the variable appears 
+# return the final result of the tracker 
+
