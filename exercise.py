d71e53c25b04fed1a950fcd826f0cf007b122afe
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2025-11-20 09:36:15 +0000

    Add exercises

---
+++ exercise.py
@@ -0,0 +1,13 @@
+# Build a small program that turns a fullname into its initials
+# eg. make_initials("Will Smith") #=> 'W.S'
+
+# Parameters
+#    - fullname, a string (eg. "Xiao Chan")
+# Returns
+#    - string (eg. 'X.C')
+# Side effects
+#    - None
+def make_initials(fullname):
+    pass
+
+print(make_initials('Will Smith'))
